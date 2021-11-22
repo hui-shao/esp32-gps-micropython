4877ee5b171712f64150168dcc7b85d832b64571
hui-shao <hui-shao@outlook.com>
2021-11-22 14:16:07 +0800

    [refactor] 封装 gps_uart.py 为 classmethod

--- gps_uart.py
+++ gps_uart.py
@@ -1,24 +1,36 @@
-import time
 import machine
 from gps_info_format import GpsInfo
 
-uart1 = machine.UART(2, baudrate=9600, rx=13, tx=12, timeout=10)
-i = 1
-while i <= 1000:
-    # uart1.write("Test " + str(i))
-    time.sleep(1)
-    print("===========================\n")
-    print("No." + str(i))
-    if uart1.any():
-        bin_data = uart1.read()
-        G = GpsInfo(bin_data.decode())
-        print("\nRaw data:")
-        print(bin_data.decode())
-        print("\nFormatted data:")
-        print(G.DT.date_str + " " + G.DT.time_ms_str)
-        print(G.P.position_f)
-    else:
-        print("\nNo data for read")
-    print()
-    print("===========================\n")
-    i += 1
+
+class GPS:
+    def __init__(self):
+        self.uart1 = machine.UART(2, baudrate=9600, rx=13, tx=12, timeout=10)
+
+    def read(self, _i: int) -> GpsInfo:
+        # self.uart1.write("Test " + str(_i))
+        print("===========================\n")
+        print("No." + str(_i))
+        if self.uart1.any():
+            bin_data = self.uart1.read()
+            gi = GpsInfo(bin_data.decode())
+            print("\nRaw data:")
+            print(bin_data.decode())
+            print("\nFormatted data:")
+            print(gi.DT.date_str + " " + gi.DT.time_ms_str)
+            print(gi.P.position_f)
+        else:
+            gi = GpsInfo("null")
+            print("\nNo data for read")
+        print()
+        print("===========================\n")
+        return gi
+
+
+if __name__ == '__main__':
+    import time
+
+    G = GPS()
+    i = 0
+    while i < 2048:
+        GI = G.read(i)
+        time.sleep(1)
