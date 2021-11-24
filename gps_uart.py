40663b8570d1b06c17203aa18b82713b0f9991de
hui-shao <hui-shao@outlook.com>
2021-11-24 21:32:30 +0800

    [fix] 修复 读取不到 gps 数据 的问题
    主要和 txd rxd 引脚号选择有关系

--- gps_uart.py
+++ gps_uart.py
@@ -4,36 +4,36 @@ from gps_info_format import GpsInfo
 
 class GPS:
     def __init__(self, is_print: bool = False):
-        self.uart1 = machine.UART(2, baudrate=9600, rx=35, tx=36, timeout=10)
+        self.uart1 = machine.UART(2, baudrate=9600, rx=23, tx=22, timeout=10)
         self.is_print = is_print
 
-    @staticmethod
-    def print_data(_i: int, _is_recv: bool, _gi, _bin_data: bytes = ""):
-        print("===========================\n")
-        print("No." + str(_i))
-        if _is_recv:
-            print("\nRaw data:")
-            print(_bin_data.decode())
-            print("\nFormatted data:")
-            print(_gi.DT.date_str + " " + _gi.DT.time_ms_str)
-            print(_gi.P.position_f_s)
-        else:
-            print("\nNo data for read")
-        print()
-        print("===========================\n")
+    # @staticmethod
+    # def print_data(_i: int, _is_recv: bool, _gi, _bin_data: bytes = ""):
+    #     print("===========================\n")
+    #     print("No." + str(_i))
+    #     if _is_recv:
+    #         print("\nRaw data:")
+    #         print(_bin_data.decode())
+    #         print("\nFormatted data:")
+    #         print(_gi.DT.date_str + " " + _gi.DT.time_ms_str)
+    #         print(_gi.P.position_f_s)
+    #     else:
+    #         print("\nNo data for read")
+    #     print()
+    #     print("===========================\n")
 
     def read(self, _i: int) -> GpsInfo:
         try:
             if self.uart1.any():
                 bin_data = self.uart1.read()
                 gi = GpsInfo(bin_data.decode())
-                self.print_data(_i, True, gi, bin_data)
+                # self.print_data(_i, True, gi, bin_data)
             else:
                 gi = GpsInfo("null")
-                self.print_data(_i, True, gi)
+                # self.print_data(_i, True, gi)
         except UnicodeError:
             gi = GpsInfo("null")
-            self.print_data(_i, True, gi)
+            # self.print_data(_i, True, gi)
         return gi
 
 
