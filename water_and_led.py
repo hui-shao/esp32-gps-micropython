e1259fc68b8586b361f1099b86029d00b372b4b3
hui-shao <hui-shao@outlook.com>
2021-11-25 12:58:54 +0800

    [fix] 修复 蜂鸣器一直响的 问题

--- water_and_led.py
+++ water_and_led.py
@@ -12,26 +12,40 @@ adc32 = machine.ADC(machine.Pin(32))
 class WaterSensor:
     @staticmethod
     def run():
-        if adc32.any():
-            # print(str(adc32.read()))
-            pass
+        # if adc32.any():
+        #     # print(str(adc32.read()))
+        #     pass
         if 0 <= adc32.read() <= 200:
-            pwm14.duty(0)
-            pwm15.duty(0)
-            pwm16.duty(255)
+            WaterSensor.led_blue()
             buzzer.value(0)
             time.sleep(0.3)
             buzzer.value(1)
             time.sleep(1)
+            buzzer.value(0)
         if 200 < adc32.read() < 400:
-            pwm14.duty(0)
-            pwm15.duty(255)
-            pwm16.duty(0)
+            WaterSensor.led_green()
         if adc32.read() >= 400:
-            pwm14.duty(255)
-            pwm15.duty(0)
-            pwm16.duty(0)
+            WaterSensor.led_red()
             buzzer.value(0)
             time.sleep(0.3)
             buzzer.value(1)
             time.sleep(1)
+            buzzer.value(0)
+
+    @staticmethod
+    def led_red():
+        pwm14.duty(255)
+        pwm15.duty(0)
+        pwm16.duty(0)
+
+    @staticmethod
+    def led_green():
+        pwm14.duty(0)
+        pwm15.duty(255)
+        pwm16.duty(0)
+
+    @staticmethod
+    def led_blue():
+        pwm14.duty(0)
+        pwm15.duty(0)
+        pwm16.duty(255)
